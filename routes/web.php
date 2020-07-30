60a51a7e22471fb91b7ed662beb30273bc3c9dec
Belal Khan <belalkhan.dev@gmail.com>
2020-07-30 23:34:59 +0600

    category manage controller add

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,29 @@ Route::middleware(['auth'])->group(function () {
         Route::prefix('admin')->group(function () {
             Route::get('/', 'DashboardController@index')->name('admin');
             Route::get('/dashboard', 'DashboardController@index')->name('admin.dashboard');
+
+            // category manage routes
+            Route::prefix('category')->group(function () {
+                Route::get('/', 'CategoryController@index')->name('admin.category.list');
+                Route::get('/create', 'CategoryController@create')->name('admin.category.create');
+                Route::post('/create', 'CategoryController@store')->name('admin.category.store');
+                Route::get('/edit/{id}', 'CategoryController@edit')->name('admin.category.edit');
+                Route::post('/edit/{id}', 'CategoryController@update')->name('admin.category.update');
+                Route::delete('/{id}/delete', 'CategoryController@destroy')->name('admin.category.delete');
+            });
+
+            // SubCategory manage routes
+            Route::prefix('sub-category')->group(function () {
+                Route::get('/', 'SubCategoryController@index')->name('admin.sub-category.list');
+                Route::get('/create', 'SubCategoryController@create')->name('admin.sub-category.create');
+                Route::post('/create', 'SubCategoryController@store')->name('admin.sub-category.store');
+                Route::get('/edit/{id}', 'SubCategoryController@edit')->name('admin.sub-category.edit');
+                Route::post('/edit/{id}', 'SubCategoryController@update')->name('admin.sub-category.update');
+                Route::delete('/{id}/delete', 'SubCategoryController@destroy')->name('admin.sub-category.delete');
+            });
         });
+
+        
 
     });
 });
